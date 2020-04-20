cd2b33d54310a1f16371faa2d21506dfeb1b86da
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-20 18:32:46 +0530

    disconnect , callUser, acceptCall functionality added

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const app = require('express')();
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
 
+const users = {}
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.send('<h1>Web RTC Signalling</h1>');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log(socket.id)
+
+    if (!users[socket.id]) {
+        users[socket.id] = socket.id;
+    }
+    socket.emit("myId", socket.id);
+    io.sockets.emit("allUsers", users);
+
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+    })
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
+    })
+
+    socket.on("acceptCall", (data) => {
+        io.to(data.to).emit('callAccepted', data.signal);
+    })
+
 });
 
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(5500, () => {
+    console.log('listening on *:5500');
 });
